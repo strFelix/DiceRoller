040b8ce03aa6ab5c4db9e5a9b9af01179cd5ea2d
Lucas Felix <lucas.felixandrade@hotmail.com.br>
2023-09-01 22:15:05 -0300

    Adicionando picker e alterando a forma de geração do número

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -10,11 +10,15 @@ public partial class MainPage : ContentPage
 	private void OnButtonClicked(object sender, EventArgs e)
 	{
 		int min = 1;
-		int max = 10;
+        //int max = 101;
 
+        var numeroSelecionado = picker.SelectedItem;
+		string numeroTxt = numeroSelecionado.ToString();
+		int max = int.Parse(numeroTxt) + 1;
+		
 		Random random = new Random();
 
-		int numeroGerado = random.Next(min, max + 1);
+		int numeroGerado = random.Next(min, max);
 		numberOutput.Text = numeroGerado.ToString();
     }
 }
