75065c8c46c92dbce3c6b4612a736083ddf74cf2
Lucas Felix <lucas.felixandrade@hotmail.com.br>
2023-09-01 21:41:02 -0300

    Adicionando botões e função de gerar número ao mesmo

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -2,23 +2,20 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void OnButtonClicked(object sender, EventArgs e)
 	{
-		count++;
+		int min = 1;
+		int max = 10;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		Random random = new Random();
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+		int numeroGerado = random.Next(min, max + 1);
+		numberOutput.Text = numeroGerado.ToString();
+    }
 }
 
