a8c1c367ba35cb0040b70ce7f5c0de6767dbce12
Lucas Felix <Lucas.felixandrade@hotmail.com.br>
2023-09-11 21:46:31 -0300

    [FEAT] Adicionei funcionalidades em uma classe, separando métodos e formas

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -7,14 +7,37 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void SortearNumero(object sender, EventArgs e)
-	{
-		//eleciona valor picker
-		var maxValue = seletorQuantidadeLados.SelectedItem;
-		//instancia do objeto gerando um numero com minimo e maximo (+1)
-		var numeroSorteado = new Random().Next(1, (int)maxValue + 1);
+    public class Dice
+    {
+        private int NumSides {get; set;}
 
-		//Mostrando o numero na tela"
+        //overload
+        public Dice() { } //metodo construtor padrão
+
+        public Dice(int numSides) //construtor com argumentos
+        {
+            NumSides = numSides; //definindo que o numero de lados é o parametro do ARGUMENTO
+        }
+
+        //gerando numero aleatório
+        public int RollDice() 
+        {
+            int random = new Random().Next(1, NumSides + 1);
+            return random;
+        }
+    }
+
+    private void OnSortearNumeroClicked(object sender, EventArgs e)
+	{
+        //Dice dice = new Dice(7); || Dice dice = new Dice(); Diferentes formas de instanciar a classe 
+        
+        //seleciona valor picker como limitador
+        Dice dice = new Dice((int)seletorQuantidadeLados.SelectedItem); //casting --> altera a forma que o parametro é enxergado (convert)
+
+		//referenciando o método de sorteio a uma variavel
+		var numeroSorteado = dice.RollDice();
+
+		//Mostrando o numero na tela
 		numberOutput.Text= numeroSorteado.ToString();
     }
 }
